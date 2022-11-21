f4a0deb51f076773adc7ea3ce404478c3f971ce4
famosab <famke.baeuerle@gmail.com>
2022-11-21 14:32:47 +0100

    Add PARAMS file, split ipynb, add utils #6

---
+++ plate_reader/PARAMS.py
@@ -0,0 +1,23 @@
+# styling of plots
+import matplotlib
+
+matplotlib.rcParams['axes.linewidth'] = 2
+matplotlib.rcParams['figure.dpi'] = 180
+matplotlib.rcParams['savefig.dpi'] = 180
+matplotlib.rcParams['lines.linewidth'] = 2 
+matplotlib.rcParams['errorbar.capsize'] = 2
+matplotlib.rcParams["axes.spines.right"] = False
+matplotlib.rcParams["axes.spines.top"] = False
+matplotlib.rcParams["xtick.major.width"] = 2
+matplotlib.rcParams["ytick.major.width"] = 2
+matplotlib.rcParams['axes.labelsize'] = 16
+matplotlib.rcParams['legend.frameon'] = False
+matplotlib.rcParams['legend.fontsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 14
+matplotlib.rcParams['ytick.labelsize'] = 14
+
+# colors and patterns
+cb = ['#377eb8', '#ff7f00', '#4daf4a','#f781bf', '#a65628', '#984ea3','#999999', '#e41a1c', '#dede00']
+pat = [".", 'x', '+']
+colorstandards = {'14':'tab:cyan', '15':'g', '16':'darksalmon', '17': 'goldenrod'}
+markers = ["D", "x", "o",  "H", "d", "p", "H",  "*"]
---
+++ plate_reader/utils.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np # for calculating standard deviation and mean
+import scipy.stats as sp # for calculating standard error
+import matplotlib.pyplot as plt
+import matplotlib
+from pylatex import Figure, NoEscape, Subsubsection
+from pylatex.utils import italic
+import os
+from sklearn import metrics
+from scipy.optimize.minpack import curve_fit
+from datetime import datetime
+import seaborn as sns
+import warnings
+
+def add_time(df):
+    m = [isinstance(i, datetime) for i in df['Time']]
+    df['timedelta'] = pd.to_timedelta(df['Time'].astype(str), errors='coerce')
+    df.loc[m, 'timedelta'] = df.loc[m, 'Time'].apply(lambda t: pd.Timestamp(str(t)) - pd.Timestamp('1899-12-31'))
+    df['Time'] = (df['timedelta'] - df['timedelta'][0])
+    df['Time'] = df['Time'].dt.total_seconds() / 60
+    df['Time'] += 15
+    return df.drop('timedelta', axis=1)
+
+def extract_columns(df, conditions, bio_rep=False, bio_rep_name=''):
+    """transforms excel table to df where the technical triplicates are averaged
+
+    Args:
+        df (pandas-df): excel from plate reader loaded
+        conditions (dict): plate_layout loaded with convert_layout()
+    """
+
+    triplicates = df.iloc[:,2:].groupby(np.arange(len(df.iloc[:,2:].columns))//3, axis=1).mean()
+
+    blank = triplicates[triplicates.columns[3::4]]
+    blank.columns = np.arange(8)
+
+    left = triplicates[triplicates.columns[::4]]
+    left.columns = np.arange(8)
+    left = left - blank
+    left = left.rename(columns=conditions)
+    left = df.iloc[:,:1].join(left, how='outer')
+    left = add_time(left)
+
+    middle = triplicates[triplicates.columns[1::4]]
+    middle.columns = np.arange(8)
+    middle = middle - blank
+    middle = middle.rename(columns=conditions)
+    middle = df.iloc[:,:1].join(middle, how='outer')
+    middle = add_time(middle)
+
+    right = triplicates[triplicates.columns[2::4]]
+    right.columns = np.arange(8)
+    right = right - blank
+    right = right.rename(columns=conditions)
+    right = df.iloc[:,:1].join(right, how='outer')
+    right = add_time(right)
+    
+    def double_std(array):
+        return np.std(array) * 2
+    
+    if bio_rep:
+        df_concat = pd.concat((left, middle, right))
+        by_row_index = df_concat.groupby(df_concat.index)
+        df_means = by_row_index.mean()
+        df_errors = by_row_index.agg([np.mean, double_std, sp.sem])
+        return_dict = {conditions['A']:left,
+                   conditions['B']:middle,
+                   conditions['C']:right,
+                   bio_rep_name: df_means}
+    
+    else:
+        return_dict = {conditions['A']:left,
+                   conditions['B']:middle,
+                   conditions['C']:right}
+        df_errors = None
+    
+    return  return_dict, df_errors
+
+def convert_layout(plate_layout_file = 'plate_layout.csv'):
+    """converts csv layout file to work with other functions
+
+    Args:
+        plate_layout_file (str): Path to csv with layout. Defaults to 'plate_layout.csv'.
+
+    Returns:
+        dict: date as keys and layout as value (entry of the row)
+    """
+    layout = pd.read_csv(plate_layout_file)
+    layout_dict = {}
+
+    for entry in layout.to_dict('records'):
+        new_dict = {}
+        for k, v in entry.items():
+            try: 
+                k = int(k)
+                new_dict[k] = v
+            except(ValueError):
+                new_dict[k] = v
+        layout_dict[new_dict['date']] = new_dict
+    
+    return layout_dict
+
+def get_info(excel_file):
+    date = excel_file[:6]
+    nice_date = date[4:] + '.' + date[2:4] + '.20' + date[:2]
+    plate_layout = convert_layout()[int(date)]
+    nrows = 0 
+    if plate_layout['duration'] == 24.0:
+        nrows = 96 #96 for 24
+    if plate_layout['duration'] == 48.0:
+        nrows = 193 #193 for 48h
+    bio_rep_name = ''
+    if plate_layout['biological replicates']:
+        bio_rep_name = str(int(plate_layout['strain']))
+    return nrows, plate_layout, bio_rep_name, nice_date
+
+def logistic(t, K, N0, r): #asadi2020
+    return K/(1+((K-N0)/N0)*np.exp(-r*t))
